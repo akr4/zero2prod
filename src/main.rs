bbb4d02038dac0a87d6c6462ee24aab223cdf540
Akira Ueda <ue@gol.com>
2021-05-24 09:56:25 +0900

    Bind random port

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::net::TcpListener;
+
 #[actix_web::main]
-pub async fn main() -> std::io::Result<()> {
-    zero2prod::run()?.await
+pub async fn main() -> Result<(), std::io::Error> {
+    zero2prod::run(TcpListener::bind("127.0.0.1:8000")?)?.await
 }
